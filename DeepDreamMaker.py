b57f198338524240deae2e2f64220ecf974f138e
Nix <pix.motion@yahoo.com>
2015-07-17 02:19:41 -0700

    first commit

---
+++ DeepDreamMaker.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+import sys
+#import DeepDreamThread
+import DeepDreamThread
+from PyQt4 import QtGui, QtCore, uic
+
+
+class Window(QtGui.QMainWindow):
+
+	inputImage = "" # input image path
+	outputLoc  = ""	# output image path
+	ddArgs = {
+		"preview-mode": True # chkPreviewMode
+	}
+
+	def __init__(self):
+		super(Window, self).__init__()
+		# gui.ui made with qt designer
+		self.ui = uic.loadUi("gui.ui", self)
+		# menu bar actions
+		self.ui.actionAbout.triggered.connect(self.aboutMsg)
+		self.ui.actionExit.triggered.connect(self.closeApp)
+		# butons
+		self.ui.btnInputFile.clicked.connect(self.openFile)
+		self.ui.btnSetOutputLoc.clicked.connect(self.setOutputLoc)
+		self.ui.btnMakeitDream.clicked.connect(self.makeItDream)
+		self.ui.btnStopDream.clicked.connect(self.stopDream)
+
+		self.ui.show()
+
+	def openFile(self):
+		"""
+			Opens a file a file dialog to pick the input image
+			A filter to only allow .png and .jpg images is used
+			TODO: check file actually exits?
+		"""
+		self.inputImage = QtGui.QFileDialog.getOpenFileName(self, "Open File", "", "Images (*.png *.jpg)")
+		if self.inputImage == "":
+			self.ui.lblInputImg.setText("No image selected")
+		else:
+			self.ui.lblInputImg.setText(self.inputImage)
+			
+		self.enableMakeitDream()
+
+	def setOutputLoc(self):
+		"""
+			Opens a file a file dialog to pick the DIRECTORY/FOLDER
+			where the proccesed image is to be saved.
+			TODO: check directory actually exits?
+		"""
+		self.outputLoc = QtGui.QFileDialog.getExistingDirectory(self, 'Select a folder:', '', QtGui.QFileDialog.ShowDirsOnly)
+		if self.outputLoc == "":
+			self.ui.lblOutputLoc.setText("No folder selected")
+		else:
+			self.ui.lblOutputLoc.setText(self.outputLoc)
+		
+		self.enableMakeitDream()
+
+	def updateConsole(self, outputTxt):
+		"""
+			Updates the "console" (QPlainTextEdit) contents
+		"""
+		self.ui.consoleContainer.appendPlainText(outputTxt)
+
+	def aboutMsg(self):
+		"""
+			Display a message box showing information about this application
+			when the user clicks the "about" menu btn
+		"""
+		msgBoxTxt = "Deep Dream Maker v0.1\n\n"
+		msgBoxTxt += "This application allows you to use Google's Deep Dream python script\n"
+		msgBoxTxt += "without needed to use a command line\n\n"
+		msgBoxTxt += "This is an open source project maintained by @overstruck\n\n"
+		msgBoxTxt += "Repository: https://github.com/OverStruck/deep-dream-maker"
+
+		msgBox = QtGui.QMessageBox()
+		msgBox.setText(msgBoxTxt)
+		msgBox.exec_()
+
+	def closeApp(self):
+		sys.exit()
+
+	def enableMakeitDream(self):
+		"""
+			Enables the main button that initializes deep dream
+			only if we have a valid input image path and an output directory
+		"""
+		if self.inputImage != "" and self.outputLoc !="":
+			self.ui.btnMakeitDream.setEnabled(True)
+		else:
+			self.ui.btnMakeitDream.setDisabled(True)
+
+	def stopDream(self):
+		"""
+			kills thread running current dream. Disables btnStopDream. Enables btnMakeitDream
+		"""
+		self.deepDreamThread.killSubProcess()
+
+		self.ui.btnMakeitDream.setEnabled(True)
+		self.ui.btnStopDream.setDisabled(True)
+
+	def makeItDream(self):
+		"""
+			Main function that calls a thread class which calls Google's deep dream method
+			We use a thread so that our user interface is not frozen and so that we can
+			update our console with information about the current proccess
+		"""
+		# disable make it dream btn
+		self.ui.btnMakeitDream.setDisabled(True)
+		# enable stop dream btn to kill process
+		self.ui.btnStopDream.setEnabled(True)
+		# need to convert from Qstring to a normal string
+		inputImg = str(self.inputImage)
+		outputLoc = str(self.outputLoc)
+
+		self.deepDreamThread = DeepDreamThread.DeepDreamThread(self, inputImg, outputLoc, self.ddArgs)
+		# connect signal to slot to listen for changes and update cosole contents
+		self.deepDreamThread.consoleUpdated.connect(self.updateConsole)
+		self.deepDreamThread.start()
+
+def main():
+	app = QtGui.QApplication([])
+	Window()
+	sys.exit(app.exec_())
+
+main()
---
+++ DeepDreamThread.py
@@ -0,0 +1,77 @@
+from PyQt4 import QtCore
+
+import os
+import time
+import signal
+from subprocess import Popen, PIPE, STDOUT
+
+class DeepDreamThread(QtCore.QThread):
+	"""
+		We use this class to keep the UI responsive by offloading the actual work
+		to proccess the image to this thread. Doing this also allows us to update our UI
+	"""
+	# signal to upate console output
+	subprocessKilled = False
+	consoleUpdated = QtCore.pyqtSignal(str)
+
+	def __init__(self, mw, inputImg, outputLoc, ddArgs):
+		super(DeepDreamThread, self).__init__(mw)
+		self.inputImg = inputImg	# input image PATH
+		self.outputLoc = outputLoc	# output directory PATH
+		self.ddArgs = ddArgs # options/arguments to pass 2 deepdream
+		# file name
+		self.fileName = self.getFileName(self.inputImg)
+		self.outputFileName = self.getOutputFileName()
+
+	def run(self):
+		"""
+			Main method automatically called.
+			We just cakk the actual deep dream method from another script
+			and we setup some variables to have some interesting info about
+			how long it takes to proccess this image
+		"""
+		startTime = time.time()
+		self.updateConsole("##### Started dreamifying \"%s\" #####" % self.fileName)
+		
+		# we run the actual deepdream code in a subprocess in order to capture any and all
+		# console output as if we were running it in a terminal
+		# notice the -u flag in our command, it is required to capture the output in real time
+		cmd = "python -u dream.py \"%s\" \"%s\"" % (self.inputImg, self.outputFileName)
+		self.ddSubProc = Popen(cmd, stdout=PIPE, stderr=STDOUT, shell=True, bufsize=1, preexec_fn=os.setsid)
+		while self.ddSubProc.poll() is None:
+			line = self.ddSubProc.stdout.readline()
+			if line:
+				self.updateConsole(line)
+		
+		#calculate proccess time
+		if not self.subprocessKilled:
+			endMsg = self.calcEndTime(startTime)
+			self.updateConsole(endMsg)
+
+	def updateConsole(self, outputTxt):
+		self.consoleUpdated.emit(outputTxt)
+
+	def calcEndTime(self, startTime):
+		timeCurrency = "seconds"
+		endTime = float( time.time() - startTime ) # in seconds
+
+		if endTime > 60.0:
+			timeCurrency = "minutes"
+			endTime = endTime / 60 # time in minutes
+
+		return "##### Finished dreamifying \"%s\" in %.2f %s #####" % (self.fileName, endTime, timeCurrency)
+
+	def getFileName(self, path):
+		return os.path.basename(path)
+
+	def getOutputFileName(self):
+		fileName = self.fileName
+		fileName = fileName.replace(".jpg", ".png")
+		fileName = fileName.replace(".jpeg", ".png")
+		return self.outputLoc + "/" + fileName
+
+	def killSubProcess(self):
+		ddSubProcId = self.ddSubProc.pid
+		os.killpg(ddSubProcId, signal.SIGTERM)
+		self.subprocessKilled = True
+		self.updateConsole("###### Deep Dream Sub Process %d killed ######" % ddSubProcId)
